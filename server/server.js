b41d17b317b3163165a279a62006c8394f0b133b
Aswin <aswinlalu645@gmail.com>
2026-02-19 12:56:39 +0530

    feat(server) : server was created

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+dotenv.config()
+
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cookieParser());
+app.use(cors({ origin : "httphttp://localhost:5173", credentials : true}))
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT,()=>{
+  console.log(`server is running on localhost : ${PORT}`)
+})  
